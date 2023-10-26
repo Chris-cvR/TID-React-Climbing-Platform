8fb10ec64616c3d11620246d45749c73c0260b30
Chris <93438981+De1ux-1@users.noreply.github.com>
2023-10-26 16:54:57 +0200

    Added simple button - Theo did this

---
+++ src/components/common/PrimaryButton.js
@@ -0,0 +1,12 @@
+import Button from 'react-bootstrap/Button';
+import "../../styles/index.css"
+
+function PrimaryButton() {
+    return (
+        <>
+            <Button id="PrimaryButton" >Create Location</Button>{' '}
+        </>
+    );
+}
+
+export default PrimaryButton;
---
+++ src/components/sections/Sidebar.js
@@ -0,0 +1,12 @@
+import PrimaryButton from "../common/PrimaryButton";
+
+function Sidebar() {
+
+    return (
+        <PrimaryButton></PrimaryButton>
+
+    );
+
+}
+
+export default Sidebar;
