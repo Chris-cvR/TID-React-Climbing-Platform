7ecfc5659e3703e72d5ca542771a7f7e4f422c08
nari <nari@itu.dk>
2023-10-30 20:24:25 +0100

    Added Comment.js

---
+++ src/components/common/Comment.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Comment(props) {
+  return (
+    <div>
+      {/* Your JSX code for the component goes here
+     - show already existing comments as a list or Map
+     - Create a text box, where you can write a comment
+     - Add a button that "adds" the text to the list of comments */}
+    </div>
+  );
+}
+
+export default Comment;
