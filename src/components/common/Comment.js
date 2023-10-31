56a9c84beb20e622bea1bcb614e152653ea0298b
nari <nari@itu.dk>
2023-10-31 22:39:31 +0100

    Added to Comment-component + created comment.css

--- src/components/common/Comment.js
+++ src/components/common/Comment.js
@@ -1,12 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
+//import "../../styles/index.css";
+import "../../styles/Comment.css";
 
-function Comment(props) {
+function Comment() {
+  // Initialize state variables using the useState hook
+  const [comments, setComments] = useState([]); // Store the list of comments
+  const [newComment, setNewComment] = useState(""); // Store the new comment being typed by the user
+
+  // Function to add a new comment to the list
+  const addComment = () => {
+    if (newComment) {
+      setComments([...comments, newComment]); // Update the list of comments with the new comment
+      setNewComment(""); // Clear the input field
+    }
+  };
+
   return (
-    <div>
-      {/* Your JSX code for the component goes here
-     - show already existing comments as a list or Map
-     - Create a text box, where you can write a comment
-     - Add a button that "adds" the text to the list of comments */}
+    <div className="comment-container">
+      <h2>Comments</h2>
+
+      {comments.map((comment, index) => (
+        <div key={index} className="posted-comment">
+          <p className="comment-title">UserName</p>
+          <p className="comment-text">{comment}</p>
+        </div> // Map through the list of comments and display each in a separate container + add "UserName" as a title for each comment
+      ))}
+      <input
+        type="text"
+        placeholder="Enter your comment here"
+        style={{ width: "500px", height: "100px" }}
+        value={newComment}
+        onChange={(e) => setNewComment(e.target.value)}
+      />
+      <button className="comment-button" onClick={addComment}>
+        Add Comment
+      </button>
     </div>
   );
 }
