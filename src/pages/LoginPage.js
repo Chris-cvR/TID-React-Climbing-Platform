d27a20aa893ab9b6d13b51176990e66ee045fa39
Chris <93438981+De1ux-1@users.noreply.github.com>
2023-11-03 00:01:15 +0100

    Added Login Info

---
+++ src/pages/LoginPage.js
@@ -0,0 +1,40 @@
+import Logo from "../assets/images/PeakPulse.png";
+
+function LoginPage() {
+
+    return (
+        <>
+            <div className="login-container">
+                <div className="login-image">
+                    <img className="login-logo"
+                        src={Logo}
+                        width="100"
+                        height="100"
+                        alt=""
+                    />
+                </div>
+
+                <p className="welcome-text"><i className="fas fa-home"></i> Welcome to PeakPulse!</p>
+
+                <div>
+                    <form>
+                        <div className="mb-3 custom-input">
+                            <input type="text" className="form-control" id="exampleInputEmail" placeholder="E-Mail" />
+                        </div>
+
+                        <div className="mb-3 custom-input">
+                            <input type="text" className="form-control" id="exampleInputPassword" placeholder="Password" />
+                        </div>
+
+                        <button type="Login" className="btn btn-primary custom-button">Login</button>
+                    </form>
+                    <h6 className="NotAMember">Not a member? <a href="/Register.html" className="link">Register</a> </h6>
+                </div>
+            </div>
+
+        </>
+    );
+}
+
+export default LoginPage;
+
