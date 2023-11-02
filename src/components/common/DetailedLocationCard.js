dfaf149d687e1935b8bd07567917914443037309
Chris <93438981+De1ux-1@users.noreply.github.com>
2023-11-02 14:38:03 +0100

    Finished Detailed location card

--- src/components/common/DetailedLocationCard.js
+++ src/components/common/DetailedLocationCard.js
@@ -2,6 +2,8 @@ import React from 'react';
 import "../../styles/index.css";
 import picture1 from "../../assets/images/Blocs&Walls.jpg"
 import { Container, Card } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHeart } from '@fortawesome/free-solid-svg-icons';
 
 
 function LocationCard() {
@@ -62,32 +64,20 @@ function LocationCard() {
                         <div className='detailed-text'>
                             Blocs & Walls, located on Amager Island, offers a wide range of climbing options. The gym features both bouldering walls and top-rope climbing routes. Whether you prefer the intense, dynamic nature of bouldering or the more endurance-focused rope climbing, you'll find suitable challenges here. Blocs & Walls, located on Amager Island, offers a wide range of climbing options. The gym features both bouldering walls and top-rope climbing routes. Whether you prefer the intense, dynamic nature of bouldering or the more endurance-focused rope climbing, you'll find suitable challenges here. Blocs & Walls, located on Amager Island, offers a wide range of climbing options. The gym features both bouldering walls and top-rope climbing routes. Whether you prefer the intense, dynamic nature of bouldering or the more endurance-focused rope climbing, you'll find suitable challenges here.
                         </div>
-                        <div className="detailed-flex-container">
+                        <div className="detailed-flex-container2">
                             <div className="detailed-content">
                                 <h8>Type</h8>
                                 <div className="detailed-text-container">
-                                    <div className="coordinate-container">
-                                        <p><b>Latitude:</b></p>
-                                        <span className="coordinate-value">55.6928</span>
-                                    </div>
-                                </div>
-
-                                <div className="detailed-text-container">
-                                    <div className="coordinate-container">
-                                        <p><b>Longitude:</b></p>
-                                        <span className="coordinate-value">12.6107</span>
-                                    </div>
+                                    <p>Gym, Bouldering</p>
                                 </div>
 
                             </div>
 
-                            <div className="detailed-content">
-                                <h8>Address</h8>
-                                <div className="detailed-text-container">
-                                    <p>Refshalevej 163D
-                                        1432, København K
-                                        Denmark
-                                    </p>
+                            <div className="detailed-likes-content">
+                                <h8>Likes</h8>
+                                <div className="coordinate-container">
+                                    <FontAwesomeIcon icon={faHeart} id="likes-heart" />
+                                    <div id="detailed-likes">10</div>
                                 </div>
                             </div>
                         </div>
