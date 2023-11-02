6bef4e4eae2d98b3daa4ca01441e73dba15bdd26
Chris <93438981+De1ux-1@users.noreply.github.com>
2023-11-02 10:42:54 +0100

    Removed sidebar for now

--- src/pages/FeedPage.js
+++ src/pages/FeedPage.js
@@ -1,7 +1,6 @@
 import Navbar from "../components/common/Navbar"
 import Footer from "../components/common/Footer";
 import LocationCard from "../components/common/LocationCard";
-import Sidebar from "../components/sections/Sidebar";
 import picture1 from "../assets/images/Blocs&Walls.jpg";
 import picture2 from "../assets/images/cphBoulders.jpg";
 
@@ -10,7 +9,6 @@ function FeedPage() {
     return (
         <>
             <Navbar></Navbar>
-            <Sidebar></Sidebar>
             <div>
                 <LocationCard picture={picture1} Title={"Blocs & Walls"}></LocationCard>
                 <LocationCard picture={picture2} Title={"Copenhagen Boulders"}></LocationCard>
