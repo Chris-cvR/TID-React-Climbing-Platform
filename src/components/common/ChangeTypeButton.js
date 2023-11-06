8eb9b2f67b23ce14ed673e21f0c96ab94cbfcf71
merleschoen <merle.schoen@hotmail.de>
2023-11-06 13:50:55 +0100

    dynamic sidebar component

--- src/components/common/ChangeTypeButton.js
+++ src/components/common/ChangeTypeButton.js
@@ -1,28 +1,38 @@
-import Dropdown from 'react-bootstrap/Dropdown';
-import "../../styles/index.css";
+import React, {useState} from 'react';
 
+function ChangeTypeButton({ TextCT, DItem1, DItem2, DItem3, type }) {
+  const [isOpen, setIsOpen] = useState(false);
 
+  const toggleDropdown = () => {
+    setIsOpen(!isOpen);
+  };
 
-function ChangeTypeButton({TextCT, DItem1, DItem2, DItem3}) {
-
-    const ButtonTextCT= TextCT;
-    const DropdownItem1= DItem1;
-    const DropdownItem2= DItem2;
-    const DropdownItem3= DItem3;
-
-    return (
-
-        <Dropdown>
-          <Dropdown.Toggle id="dropdown-basic">{ButtonTextCT}</Dropdown.Toggle>
-    
-          <Dropdown.Menu>
-            <Dropdown.Item href="#/action-1">{DropdownItem1}</Dropdown.Item>
-            <Dropdown.Item href="#/action-2">{DropdownItem2}</Dropdown.Item>
-            <Dropdown.Item href="#/action-3">{DropdownItem3}</Dropdown.Item>
-          </Dropdown.Menu>
-        </Dropdown>
-      );
-    }
-
+  return (
+    <div className="change-type-button dropdown">
+      <button
+        className='btn btn-secondary dropdown-toggle'
+        type='button'
+        id='dropdownMenuButton'
+        data-toggle='dropdown'
+        aria-haspopup='true'
+        aria-expanded='false'
+        onClick={toggleDropdown}
+      >
+        {TextCT}
+      </button>
+      <div className={`dropdown-menu${isOpen ? ' show' : ''}`} aria-labelledby='dropdownMenuButton'>
+        <label>
+          <input type={type} name="type" value={DItem1} /> {DItem1}
+        </label>
+        <label>
+          <input type={type} name="type" value={DItem2} /> {DItem2}
+        </label>
+        <label>
+          <input type={type} name="type" value={DItem3} /> {DItem3}
+        </label>
+      </div>
+    </div>
+  );
+}
 
 export default ChangeTypeButton;
