186c66de7cf89bc2a8a321785bef2e574bd1eb73
merleschoen <merle.schoen@hotmail.de>
2023-11-07 21:17:21 +0100

    better layout, but still messy

--- src/components/common/CreateLocation.js
+++ src/components/common/CreateLocation.js
@@ -7,12 +7,12 @@ import SecondaryButton from './SecondaryButton';
 
 const CreateLocation = ({ show, handleClose }) => {
     const [formData, setFormData] = useState([
-        {id: 1, label: 'Latitude', type: 'number', value: ''},
-        {id: 2, label: 'Longitude', type: 'number', value: ''},
-        {id: 3, label: 'Title', type: 'text', value: ''},
-        {id: 4, label: 'Experience Level', type: 'radio', value: '', options: ['Beginner', 'Intermediate', 'Advanced']},
-        {id: 5, label: 'Description', type: 'text', value: ''},
-        {id: 6, label: 'Type', type: 'checkbox', value: '', options: ['Gym-Lead', 'Outdoor-Boulder', 'Gym-Boulder']},
+        {id: 1, label: 'Title', type: 'text', value: ''},
+        {id: 2, label: 'Latitude', type: 'number', value: ''},
+        {id: 3, label: 'Longitude', type: 'number', value: ''},
+        {id: 4, label: 'Type', type: 'checkbox', value: '', options: ['Gym-Lead', 'Outdoor-Boulder', 'Gym-Boulder']},
+        {id: 5, label: 'Experience Level', type: 'radio', value: '', options: ['Beginner', 'Intermediate', 'Advanced']},
+        {id: 6, label: 'Description', type: 'text', value: ''},  
         {id: 7, label: 'Picture', type: 'file', value: ''},
     ]);
 
@@ -22,7 +22,6 @@ const CreateLocation = ({ show, handleClose }) => {
         );
         setFormData(updatedFormData);
     }
-
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -37,82 +36,103 @@ const CreateLocation = ({ show, handleClose }) => {
         </Modal.Header>
         <Modal.Body>
             <Form onSubmit={handleSubmit}>
-                <div className="row">
-                        <div className="col-md-4">
-                            {formData.slice(0, 3).map(item => (
-                            <Form.Group controlId={`formControl_${item.id}`} key={item.id}>
-                                <Form.Label>{item.label}</Form.Label>
+                <div className="row w-100">
+                        <Form.Group controlId="formControl_1">
+                            <Form.Label>Title</Form.Label>
                                 <FormControl
-                                type={item.type}
-                                placeholder={`Enter ${item.label}`}
-                                value={item.value}
-                                onChange={(e) => handleInputChange(item.id, e)}
+                                    type="text"
+                                    placeholder="Enter Title"
+                                    value={formData[0].value}
+                                    onChange={(e) => handleInputChange(1, e)}
                                 />
-                            </Form.Group>
-                            ))}
-                        </div>
-                        <div className="col-md-4">
-                            {formData.slice(3, 5).map(item => (
-                            <Form.Group controlId={`formControl_${item.id}`} key={item.id}>
-                                <Form.Label>{item.label}</Form.Label>
-                                {item.type === 'radio' ? (
-                                item.options.map(option => (
-                                    <Form.Check 
-                                    type="radio" 
-                                    label={option} 
-                                    key={option}
-                                    name={`radioGroup_${item.id}`}
-                                    value={option}
-                                    onChange={(e) => handleInputChange(item.id, e)}
-                                    />
-                                ))
-                                ) : (
+                        </Form.Group>
+                </div>
+                <div className='container mt-2'>
+                <div className="row w-100">
+                    <div className="col-md-4">
+                        <Form.Group controlId="formControl_2">
+                            <Form.Label>Latitude</Form.Label>
                                 <FormControl
-                                    type={item.type}
-                                    placeholder={`Enter ${item.label}`}
-                                    value={item.value}
-                                    onChange={(e) => handleInputChange(item.id, e)}
+                                    type="number"
+                                    placeholder="Enter Latitude"
+                                    value={formData[1].value}
+                                    onChange={(e) => handleInputChange(2, e)}
                                 />
-                                )}
-                            </Form.Group>
-                            ))}
-                        </div>
-                        <div className="col-md-4">
-                            {formData.slice(5, 7).map(item => (
-                            <Form.Group controlId={`formControl_${item.id}`} key={item.id}>
-                                <Form.Label>{item.label}</Form.Label>
-                                {item.type === 'checkbox' ? (
-                                item.options.map(option => (
-                                    <Form.Check 
-                                    type="checkbox" 
-                                    label={option} 
-                                    key={option}
-                                    onChange={(e) => handleInputChange(item.id, e)}
-                                    />
-                                ))
-                                ) : item.type === 'file' ? (
-                                <FormControl 
-                                    type="file" 
-                                    onChange={(e) => handleInputChange(item.id, e)}
-                                />
-                                ) : (
+                        </Form.Group>
+                        <Form.Group controlId="formControl_3">
+                            <Form.Label>Longitude</Form.Label>
                                 <FormControl
-                                    type={item.type}
-                                    placeholder={`Enter ${item.label}`}
-                                    value={item.value}
-                                    onChange={(e) => handleInputChange(item.id, e)}
+                                    type="number"
+                                    placeholder="Enter Longitude"
+                                    value={formData[2].value}
+                                    onChange={(e) => handleInputChange(3, e)}
                                 />
-                                )}
-                            </Form.Group>
-                            ))}
-                        </div>
+                        </Form.Group>
                     </div>
+                    <div className="col-md-4 border rounded">
+                        <Form.Group controlId="formControl_4">
+                            <Form.Label>Type</Form.Label>
+                                {formData[3].options.map(option => (
+                                    <Form.Check
+                                    type="checkbox"
+                                    label={option}
+                                    key={option}
+                                    onChange={(e) => handleInputChange(4, e)}
+                                    />
+                                ))}
+                        </Form.Group>
+                    </div>
+                    <div className="col-md-4 border rounded">
+                        <Form.Group controlId="formControl_5">
+                            <Form.Label>Experience Level</Form.Label>
+                                {formData[4].options.map(option => (
+                                    <Form.Check
+                                    type="radio"
+                                    label={option}
+                                    key={option}
+                                    name="radioGroup_5"
+                                    value={option}
+                                    onChange={(e) => handleInputChange(5, e)}
+                                    />
+                                ))}
+                        </Form.Group>
+                    </div>
+                </div>
+                </div>
+                <div className="row w-100">
+                    <Form.Group controlId="formControl_6">
+                        <Form.Label>Description</Form.Label>
+                            <FormControl
+                                type="text"
+                                placeholder="Enter Description"
+                                value={formData[5].value}
+                                onChange={(e) => handleInputChange(6, e)}
+                            />
+                    </Form.Group>
+                </div>
+                <div className="row">
+                    <div className="col-md-8">
+                        <Form.Group controlId="formControl_7">
+                            <Form.Label>Picture</Form.Label>
+                                <FormControl
+                                    type="file"
+                                    onChange={(e) => handleInputChange(7, e)}
+                                />
+                        </Form.Group>
+                    </div>
+                </div>
             </Form>
         </Modal.Body>
         <Modal.Footer>
+            <div className='container'>
             <div className="row">
-                <PrimaryButton type='submit' Text={"Save Changes"} onClick={handleClose}></PrimaryButton>
-                <SecondaryButton Text={"Cancel"} onClick={handleClose}></SecondaryButton>
+                <div className="col">
+                    <SecondaryButton Text={"Cancel"} onClick={handleClose}></SecondaryButton>
+                </div>
+                <div className="col">
+                    <PrimaryButton type='submit' Text={"Save Changes"} onClick={handleClose}></PrimaryButton>
+                </div>
+            </div>
             </div>
         </Modal.Footer>
       </Modal>
