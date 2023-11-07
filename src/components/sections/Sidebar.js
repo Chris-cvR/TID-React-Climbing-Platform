c82448783f4f0c2dd53c1da32f159e4cd58dfa93
merleschoen <merle.schoen@hotmail.de>
2023-11-07 15:04:02 +0100

    changed "change location" to "change country"

--- src/components/sections/Sidebar.js
+++ src/components/sections/Sidebar.js
@@ -12,7 +12,7 @@ function Sidebar({handleShow}) {
         <ul>
           <PrimaryButton Text={"Create Location"} onClick={handleShow}></PrimaryButton>
           <div className='dropdown-buttons'>
-            <ChangeLocationButton TextCL={"Change Location"} DItem1={"Denmark"} DItem3={"Germany"} DItem2={"Norway"} type="radio" />
+            <ChangeLocationButton TextCL={"Change Country"} DItem1={"Denmark"} DItem3={"Germany"} DItem2={"Norway"} type="radio" />
             <ChangeTypeButton TextCT={"Change Type"} DItem1={"Gym-Lead"} DItem3={"Gym-Bouler"} DItem2={"Outdoor-Boulder"} type="checkbox" />
           </div>
         </ul>
