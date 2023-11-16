a9f80497b4c48c2d38a6e43205f82ca10743fa4f
Chris <93438981+De1ux-1@users.noreply.github.com>
2023-11-16 10:55:15 +0100

    Added Parse backend

--- src/index.js
+++ src/index.js
@@ -2,6 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/index.css';
 import App from './App';
+import Parse from 'parse/dist/parse.min.js';
+
+const PARSE_APPLICATION_ID = 'SkB4EbitycaKhaamgOXH6VSIF8v9wALZX8BdrVAY';
+const PARSE_HOST_URL = 'https://parseapi.back4app.com/';
+const PARSE_JAVASCRIPT_KEY = 'm5BftvHxXy2m3GdT1OhQL32BVIIBWp1WjESfiosZ';
+Parse.initialize(PARSE_APPLICATION_ID, PARSE_JAVASCRIPT_KEY);
+Parse.serverURL = PARSE_HOST_URL;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
