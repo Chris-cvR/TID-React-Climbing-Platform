b61f99585d1a48ba264b23be661c0df7f5cef433
merleschoen <merle.schoen@hotmail.de>
2023-11-17 13:46:52 +0100

    cleaned up the register page

--- src/pages/RegistrationPage.js
+++ src/pages/RegistrationPage.js
@@ -13,6 +13,7 @@ export const RegistrationPage = () => {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
     const [experience, setExperience] = useState('');
+    const [errorMessage,setErrorMessage] = useState('');
 
     const navigate = useNavigate();
 
@@ -42,14 +43,18 @@ export const RegistrationPage = () => {
             return true;
         } catch (error) {
             // signUp can fail if any parameter is blank or failed an uniqueness check on the server
-            alert(`Error! ${error}`);
+            setErrorMessage('The password must contain at least one special character and uppercase letter.');
             return false;
         }
     };
 
+    const clearErrorMessage = () => {
+        setErrorMessage('');
+    }
+
     return (
         <div style={{ backgroundImage: `url(${image})`, backgroundSize: 'cover', backgroundPosition: 'center', height: '100vh', color: 'white' }}>
-            <div>
+            <div className='register-container'>
                 <div className="logo-welcome">
                     <img
                         src={Logo}
@@ -57,36 +62,46 @@ export const RegistrationPage = () => {
                         width="150"
                         height="150"
                     />
-
                     <p className="welcome-text">Join the PeakPulse community!</p>
                 </div>
-                <div className="register-container">
-                    <div className="form_wrapper">
-                        <Input
-                            value={username}
-                            onChange={(event) => setUsername(event.target.value)}
-                            placeholder="Username"
-                            size="large"
-                            className="form_input"
-                        />
-                        <Input
-                            value={password}
-                            onChange={(event) => setPassword(event.target.value)}
-                            placeholder="Password"
-                            size="large"
-                            type="password"
-                            className="form_input"
-                        />
-                        <label className='proficiency-selector'>
-                            Proficiency Level:
-                            <select value={experience} onChange={(e) => setExperience(e.target.value)}>
-                                <option value=""></option>
-                                <option value="beginner">Beginner</option>
-                                <option value="intermediate">Intermediate</option>
-                                <option value="advanced">Advanced</option>
-                            </select>
-                        </label>
+                <div className="register-form-container" style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                    <div className='register-input-fields'>
+                    <Input
+                        value={username}
+                        onChange={(event) => {
+                            setUsername(event.target.value);
+                            clearErrorMessage();
+                            }
+                        }
+                        placeholder="Username"
+                        size="large"
+                        className="custom-input"
+                        style={{marginTop: '4%'}}
+                    />
+                    <Input
+                        value={password}
+                        onChange={(event) => {
+                            setPassword(event.target.value);
+                            clearErrorMessage();
+                            }
+                        }
+                        placeholder="Password"
+                        size="large"
+                        type="password"
+                        className="custom-input"
+                        style={{marginTop: '4%'}}
+                    />
                     </div>
+                    <label className='proficiency-selector' style={{marginBottom: '20px'}}>
+                        Proficiency Level:
+                        <select value={experience} onChange={(e) => setExperience(e.target.value)}>
+                            <option value=""></option>
+                            <option value="beginner">Beginner</option>
+                            <option value="intermediate">Intermediate</option>
+                            <option value="advanced">Advanced</option>
+                        </select>
+                    </label>
+                    {errorMessage && <p className='error-message'>{errorMessage}</p>}
                     <Button onClick={() => doUserRegistration()} className="form-button" size="large"> Sign Up </ Button>
                     <h6 className="member-status"> Already a member? <NavLink className="nav-link" to="/login">Login</NavLink></h6>
                 </div>
