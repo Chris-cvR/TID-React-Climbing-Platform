2e746f23c9d5e29fc5b4c826461f034ffb4d3f72
nari <nari@itu.dk>
2023-11-18 15:52:36 +0100

    Added functionality to hashtags in Comment.js

--- src/components/common/Comment.js
+++ src/components/common/Comment.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "../../styles/index.css";
 import { Button } from "antd";
 import Parse from "parse/dist/parse.min.js";
@@ -7,12 +7,32 @@ function Comment() {
   // Initialize state variables using the useState hook
   const [comments, setComments] = useState([]); // Store the list of comments
   const [newComment, setNewComment] = useState(""); // Store the new comment being typed by the user
+  const [hashtags, setHashtags] = useState([]); // Keeping track of available hashtags
+  const [selectedHashtags, setSelectedHashtags] = useState([]); // Keeping track of selected hashtags
+  const [checkedStatus, setCheckedStatus] = useState({});
+
+  useEffect(() => {
+    const fetchHashtags = async () => {
+      const Hashtags = Parse.Object.extend("Hashtags");
+      const query = new Parse.Query(Hashtags);
+
+      try {
+        const hashtagResults = await query.find();
+        setHashtags(hashtagResults);
+      } catch (error) {
+        console.error(`Error while fetching Hashtags: ${error}`);
+      }
+    };
+
+    // Call the fetch function
+    fetchHashtags();
+  }, []);
 
   // Function to add a new comment to the list
   const addComment = async () => {
+    //Fetch data of current user
+    const currentUser = Parse.User.current();
     if (newComment) {
-      //Fetch data of current user
-      const currentUser = Parse.User.current();
       //Create Parse object for the Comment class and save it to the database
       const Comment = Parse.Object.extend("Comment");
       const commentToSave = new Comment();
@@ -20,15 +40,8 @@ function Comment() {
       commentToSave.set("CommentText", newComment);
       commentToSave.set("UserID", currentUser);
       //commentToSave.set('LocationID', currentLocation);
-      //commentToSave.set('HashtagID', checkedHashtags);
-
-      if (currentUser) {
-        commentToSave.set("username", currentUser.get("username")); //get the username of the current user
-        commentToSave.set("experience", currentUser.get("experience"));
-      } else {
-        commentToSave.set("username", ""); //if the user does not exist
-        commentToSave.set("experience", "");
-      }
+      const relation = commentToSave.relation("HashtagID");
+      selectedHashtags.forEach((hashtag) => relation.add(hashtag));
 
       try {
         const savedComment = await commentToSave.save();
@@ -37,17 +50,36 @@ function Comment() {
         const commentObject = {
           id: savedComment.id,
           text: newComment,
-          username: savedComment.get("username"),
-          experience: savedComment.get("experience"),
-          tags: "#Safety #Experience",
+          username: currentUser.get("username"),
+          experience: currentUser.get("experience"),
+          tags: selectedHashtags
+            .map((hashtag) => "#" + hashtag.get("Name"))
+            .join(" "), //added tags to the comment
         };
 
         setComments([...comments, commentObject]); // Update the list of comments with the new comment
         setNewComment(""); // Clear the input field
+        setSelectedHashtags([]); // Clear selected hashtags
+        setCheckedStatus({});
       } catch (error) {
         console.log(`Could not add comment. Error code: ${error}`);
       }
     }
+  };
+  // Call whenever Checkbox state changes
+  const handleCheckboxChange = (event, hashtagObj) => {
+    if (event.target.checked) {
+      setSelectedHashtags([...selectedHashtags, hashtagObj]);
+    } else {
+      setSelectedHashtags(
+        selectedHashtags.filter((tag) => tag.id !== hashtagObj.id)
+      );
+    }
+    // Record the checked status for specific checkbox
+    setCheckedStatus({
+      ...checkedStatus,
+      [hashtagObj.id]: event.target.checked,
+    });
   };
 
   return (
@@ -70,18 +102,16 @@ function Comment() {
       />
       <div className="checkbox-container">
         Hashtags:&nbsp;
-        <label className="checkbox-hashtag">
-          <input type="checkbox" />
-          #Safety&nbsp;&nbsp;&nbsp;
-        </label>
-        <label className="checkbox-hashtag">
-          <input type="checkbox" />
-          #Experience&nbsp;&nbsp;&nbsp;
-        </label>
-        <label className="checkbox-hashtag">
-          <input type="checkbox" />
-          #Experience&nbsp;&nbsp;&nbsp;
-        </label>
+        {hashtags.map((hashtagObj) => (
+          <label key={hashtagObj.id} className="checkbox-hashtag">
+            <input
+              type="checkbox"
+              checked={checkedStatus[hashtagObj.id] || false}
+              onChange={(event) => handleCheckboxChange(event, hashtagObj)}
+            />
+            {"#" + hashtagObj.get("Name")}&nbsp;&nbsp;&nbsp;
+          </label>
+        ))}
       </div>
 
       <Button onClick={addComment} className="form-button" id="comment-button">
