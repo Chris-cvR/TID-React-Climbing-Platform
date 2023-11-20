ba4494452aa76ac75207b977f85c9dc36eba3ec5
merleschoen <merle.schoen@hotmail.de>
2023-11-20 12:56:36 +0100

    updated code to parse location data to db

--- src/components/common/CreateLocation.js
+++ src/components/common/CreateLocation.js
@@ -4,17 +4,20 @@ import Form from 'react-bootstrap/Form';
 import FormControl from 'react-bootstrap/FormControl';
 import SecondaryButton from './SecondaryButton';
 import { Button } from 'antd';
+import Parse from 'parse/dist/parse.min.js';
 
 const CreateLocation = ({ show, handleClose }) => {
     const [formData, setFormData] = useState([
         { id: 1, label: 'Title', type: 'text', value: '' },
         { id: 2, label: 'Latitude', type: 'number', value: '' },
         { id: 3, label: 'Longitude', type: 'number', value: '' },
-        { id: 4, label: 'Type', type: 'checkbox', value: '', options: ['Gym-Lead', 'Outdoor-Boulder', 'Gym-Boulder'] },
-        { id: 5, label: 'Experience Level', type: 'radio', value: '', options: ['Beginner', 'Intermediate', 'Advanced'] },
-        { id: 6, label: 'Description', type: 'text', value: '' },
-        { id: 7, label: 'Picture', type: 'file', value: '' },
+        { id: 4, label: 'Country', type: 'text', value: ''},
+        { id: 5, label: 'Type', type: 'checkbox', value: '', options: ['Alpine', 'Boulder', 'Cliff', 'Freeclimb', 'Gym', 'Horizontal', 'Ice', 'Indoor', 'Lead', 'Outdoor', 'Speedclimb', 'Sport', 'Urban'] },
+        { id: 6, label: 'Difficulty', type: 'radio', value: '', options: ['Beginner', 'Intermediate', 'Advanced'] },
+        { id: 7, label: 'Description', type: 'text', value: '' },
+        { id: 8, label: 'Picture', type: 'file', value: null },
     ]);
+
 
     function valueFor(label) {
         const field = formData.find(item => item.label === label);
@@ -22,8 +25,9 @@ const CreateLocation = ({ show, handleClose }) => {
     }
 
     const handleInputChange = (id, e) => {
+        const value = id === 8 ? e.target.files[0] : e.target.value;
         const updatedFormData = formData.map(item =>
-            item.id === id ? { ...item, value: e.target.value } : item
+            item.id === id ? { ...item, value: value } : item
         );
         setFormData(updatedFormData);
     }
@@ -31,6 +35,74 @@ const CreateLocation = ({ show, handleClose }) => {
     const handleSubmit = (e) => {
         e.preventDefault();
         //code for submitting data to API
+        const Location = Parse.Object.extend("Location");
+        const newLocation = new Location();
+
+        const imageFile = valueFor("Picture");
+        const reader = new FileReader();
+
+        reader.onloadend = function () {
+            const dataUri = reader.result;
+            const imageFileName = imageFile.name;
+
+            let parseFile = new Parse.File(imageFileName, { base64: dataUri });
+
+            parseFile.save().then(savedFile => {
+                console.log('File saved: ' + savedFile);
+
+                newLocation.set("Name", valueFor("Title"));
+                newLocation.set("Latitude", Number(valueFor("Latitude")));
+                newLocation.set("Longitude", Number(valueFor("Longitude")));
+                newLocation.set("Type", valueFor("Type"));
+                newLocation.set("Difficulty", valueFor("Difficulty"));
+                newLocation.set("Description", valueFor("Description"));
+                newLocation.set("Picture", savedFile);
+
+                // Assume that "Countries" class has an object with objectId equals to the CountryID
+                var Countries = Parse.Object.extend("Countries");
+                var query = new Parse.Query(Countries);
+                console.log(valueFor("Country"));
+                query.equalTo("Country", valueFor("Country")); // first "Countyr" is the column in the Countries table
+                query.first().then((country) => {
+                if (!country) {
+                    // Country doesn't exist. Create a new one.
+                    country = new Countries();
+                    country.set("Country", valueFor("Country"));
+                    return country.save();
+                } else {
+                    // Country already exists, do nothing.
+                    return country;
+                }
+                }).then((country) => {
+                // Now, country is either the existing country, or a new one created above. 
+
+                newLocation.set("Country", country); // Set pointer to Country object
+
+                let loggedInUser = Parse.User.current();
+                if(loggedInUser){
+                    newLocation.set("UserID", loggedInUser);
+                }else{
+                    console.log("No user logged in")
+                }
+                
+                newLocation.save().then(
+                    (result) => {
+                        if (typeof document !== 'undefined') console.log(`Location created succesfully: ${JSON.stringify(result)}`);
+                    },
+                    (error) => {
+                        if (typeof document !== 'undefined') console.log(`Error while creating Location: ${JSON.stringify(error)}`);
+                    }
+                );
+                }).catch(error => {
+                    console.error('Error while saving file: ', error);
+                });
+            });
+        };
+        reader.onerror = function (error) {
+            console.log('Error: ', error);
+        };
+
+        reader.readAsDataURL(imageFile);
     }
 
     return (
@@ -73,31 +145,39 @@ const CreateLocation = ({ show, handleClose }) => {
                                             onChange={(e) => handleInputChange(3, e)}
                                         />
                                     </Form.Group>
-                                </div>
-                                <div className="col-md-4 border rounded">
                                     <Form.Group controlId="formControl_4">
-                                        <Form.Label>Type</Form.Label>
-                                        {formData.find(item => item.label === "Type").options.map(option => (
-                                            <Form.Check
-                                                type="checkbox"
-                                                label={option}
-                                                key={option}
-                                                onChange={(e) => handleInputChange(4, e)}
-                                            />
-                                        ))}
+                                        <Form.Label>Country</Form.Label>
+                                        <FormControl
+                                            type="text"
+                                            placeholder="Enter Country"
+                                            value={valueFor("Country")}
+                                            onChange={(e) => handleInputChange(4, e)}
+                                        />
                                     </Form.Group>
                                 </div>
                                 <div className="col-md-4 border rounded">
                                     <Form.Group controlId="formControl_5">
-                                        <Form.Label>Experience Level</Form.Label>
-                                        {formData.find(item => item.label === "Experience Level").options.map(option => (
+                                        <Form.Label>Type</Form.Label>
+                                        <Form.Select onChange={(e) => handleInputChange(5, e)}>
+                                            {formData.find(item => item.label === "Type").options.map(option => (
+                                                <option key={option} value={option}>
+                                                    {option}
+                                                </option>
+                                        ))}
+                                        </Form.Select>
+                                    </Form.Group>
+                                </div>
+                                <div className="col-md-4 border rounded">
+                                    <Form.Group controlId="formControl_6">
+                                        <Form.Label>Difficulty</Form.Label>
+                                        {formData.find(item => item.label === "Difficulty").options.map(option => (
                                             <Form.Check
                                                 type="radio"
                                                 label={option}
                                                 key={option}
                                                 name="radioGroup_5"
                                                 value={option}
-                                                onChange={(e) => handleInputChange(5, e)}
+                                                onChange={(e) => handleInputChange(6, e)}
                                             />
                                         ))}
                                     </Form.Group>
@@ -105,23 +185,23 @@ const CreateLocation = ({ show, handleClose }) => {
                             </div>
                         </div>
                         <div className="row w-100">
-                            <Form.Group controlId="formControl_6">
+                            <Form.Group controlId="formControl_7">
                                 <Form.Label>Description</Form.Label>
                                 <FormControl
                                     type="text"
                                     placeholder="Enter Description"
                                     value={valueFor("Description")}
-                                    onChange={(e) => handleInputChange(6, e)}
+                                    onChange={(e) => handleInputChange(7, e)}
                                 />
                             </Form.Group>
                         </div>
                         <div className="row">
                             <div className="col-md-8">
-                                <Form.Group controlId="formControl_7">
+                                <Form.Group controlId="formControl_8">
                                     <Form.Label>Picture</Form.Label>
                                     <FormControl
                                         type="file"
-                                        onChange={(e) => handleInputChange(7, e)}
+                                        onChange={(e) => handleInputChange(8, e)}
                                     />
                                 </Form.Group>
                             </div>
@@ -135,7 +215,7 @@ const CreateLocation = ({ show, handleClose }) => {
                                 <SecondaryButton Text={"Cancel"} onClick={handleClose}></SecondaryButton>
                             </div>
                             <div className="col">
-                                <Button type='submit' onClick={handleClose} className="form-button" size="large"> Save Changes </ Button>
+                                <Button type='submit' onClick={handleSubmit} className="form-button" size="large"> Save Changes </ Button>
 
                             </div>
                         </div>
