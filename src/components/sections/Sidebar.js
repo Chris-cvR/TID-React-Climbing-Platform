3e015398e7120e6849c9970b322d3aab15ab57c7
Chris <93438981+De1ux-1@users.noreply.github.com>
2023-11-23 15:26:10 +0100

    Css Fixed

--- src/components/sections/Sidebar.js
+++ src/components/sections/Sidebar.js
@@ -11,8 +11,12 @@ function Sidebar({ handleShow }) {
         <ul>
           <Button onClick={handleShow} className="form-button" size="large"> Create Location </ Button>
           <div className='dropdown-buttons-sidebar'>
-            <DropdownButton id="option-button" parseClassName='Countries' columnName='Country' placeHolderText='Countries'></DropdownButton> 
-            <DropdownButton id="option-button" parseClassName='Types' columnName='TypeName' placeHolderText='Type'></DropdownButton>
+            <div className='dropdown-button-group'>
+              <DropdownButton parseClassName='Countries' columnName='Country' placeHolderText='Countries'></DropdownButton>
+            </div>
+            <div className='dropdown-button-group'>
+              <DropdownButton parseClassName='Types' columnName='TypeName' placeHolderText='Type'></DropdownButton>
+            </div>
           </div>
         </ul>
       </nav>
