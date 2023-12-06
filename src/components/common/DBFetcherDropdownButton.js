2b36c3c7313f43cfe457bc8c8253607291aa55ef
Chris <93438981+De1ux-1@users.noreply.github.com>
2023-12-06 17:43:15 +0100

    Added filtering functionality

--- src/components/common/DBFetcherDropdownButton.js
+++ src/components/common/DBFetcherDropdownButton.js
@@ -2,8 +2,9 @@ import React, { useState, useEffect } from 'react';
 import { Select } from 'antd';
 import Parse from 'parse/dist/parse.min.js';
 
-const DBFetcherDropdownButton = ({ parseClassName, columnName, placeHolderText }) => {
+const DBFetcherDropdownButton = ({ parseClassName, columnName, placeHolderText, onSelect }) => {
     const [options, setOptions] = useState([]);
+    const [selectedValue, setSelectedValue] = useState(null);
 
     useEffect(() => {
         const fetchOptions = async () => {
@@ -17,7 +18,10 @@ const DBFetcherDropdownButton = ({ parseClassName, columnName, placeHolderText }
                     value: item.get(columnName),
                 }));
 
-                setOptions(optionList);
+                // Add the "None" option to the beginning of the options array
+                const finalOptions = [{ value: 'None' }, ...optionList];
+                setOptions(finalOptions);
+
                 return true;
             } catch (error) {
                 alert(`Error! ${error.message}`);
@@ -29,19 +33,21 @@ const DBFetcherDropdownButton = ({ parseClassName, columnName, placeHolderText }
     }, [parseClassName, columnName]);
 
     const handleChange = (value) => {
-        console.log(`Selected values: ${value}`);
+        // If "None" is selected, set the selectedValue to null
+        setSelectedValue(value === 'None' ? null : value);
+        onSelect(value); // Notify parent component about the change
     };
 
     return (
         <Select
             className='custom-dropdown'
             size='large'
-            mode="tags"
             style={{
                 width: '100%',
             }}
             placeholder={placeHolderText}
             onChange={handleChange}
+            value={selectedValue}
             options={options}
         />
     );
