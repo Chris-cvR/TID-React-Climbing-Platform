8884e4530a61b85f64dd70c0064ef20c698cf6b9
Chris <93438981+De1ux-1@users.noreply.github.com>
2024-01-08 16:37:10 +0100

    Changed database

--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ import './styles/index.css';
 import App from './App';
 import Parse from 'parse/dist/parse.min.js';
 
-const PARSE_APPLICATION_ID = 'SkB4EbitycaKhaamgOXH6VSIF8v9wALZX8BdrVAY';
+const PARSE_APPLICATION_ID = 'b0MLiigoTu4FyQg7OoaELrGobFG3Qa9IwRkL8qRB';
 const PARSE_HOST_URL = 'https://parseapi.back4app.com/';
-const PARSE_JAVASCRIPT_KEY = 'm5BftvHxXy2m3GdT1OhQL32BVIIBWp1WjESfiosZ';
+const PARSE_JAVASCRIPT_KEY = 'NyIHUIqJKnMhZdpq6JKhwuMjTB8Wzg1Vy7pUmm36';
 
 Parse.initialize(PARSE_APPLICATION_ID, PARSE_JAVASCRIPT_KEY);
 Parse.serverURL = PARSE_HOST_URL;
